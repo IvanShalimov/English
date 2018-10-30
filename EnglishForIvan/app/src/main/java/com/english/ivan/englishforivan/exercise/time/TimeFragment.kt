47de5970e6539de590993699e3d9d3de5e4fb3c8
Ivan <gogi-mogel@mail.ru>
2018-10-30 20:54:06 +0300

    add time exercise (version 1)

--- EnglishForIvan/app/src/main/java/com/english/ivan/englishforivan/exercise/time/TimeFragment.kt
+++ EnglishForIvan/app/src/main/java/com/english/ivan/englishforivan/exercise/time/TimeFragment.kt
@@ -7,16 +7,40 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.english.ivan.englishforivan.R
+import kotlinx.android.synthetic.main.fragment_time.view.*
+import java.util.*
 
 class TimeFragment : Fragment() {
+
+    lateinit var root:View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time, container, false)
+        root = inflater.inflate(R.layout.fragment_time, container, false)
+
+        root.generate_button.setOnClickListener { root.clock_image.setImageResource(generate()) }
+        return root
     }
 
 
+    private fun generate():Int{
+        return when(Random().nextInt(12)){
+            0 -> return R.drawable.clock1
+            1 -> return R.drawable.clock2
+            2 -> return R.drawable.clock3
+            3 -> return R.drawable.clock4
+            4 -> return R.drawable.clock5
+            5 -> return R.drawable.clock6
+            6 -> return R.drawable.clock7
+            7 -> return R.drawable.clock8
+            8 -> return R.drawable.clock9
+            9 -> return R.drawable.clock10
+            10 -> return R.drawable.clock11
+            11 -> return R.drawable.clock12
+
+            else -> 0
+        }
+    }
 }
